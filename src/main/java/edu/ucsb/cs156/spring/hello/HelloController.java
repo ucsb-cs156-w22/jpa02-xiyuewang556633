62230581164a5570eb854f45e268b8410012415e
Xiyue Wang <xiyuewang@umail.ucsb.edu>
2022-01-13 15:31:20 -0800

    fixed

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Welcome, Cindy!!!";
+        return "Greetings from xiyuewang!";
     }
     
 }
